195a98446c2a57c8d29141a3a9fa665550a94f41
Bodzionek <pebe83@gmail.com>
2025-09-10 22:27:24 +0200

    Updated styling

--- space.js
+++ space.js
@@ -3,14 +3,54 @@ document.addEventListener("DOMContentLoaded", function () {
   waves.start();
 });
 
-const wavesColors = [
-  "#010a16ff",
-  "#031227ff",
-  "#081b37ff",
-  "#0e2850ff",
-  "#122d56ff",
-  "#153565ff",
-];
+const colorSets = {
+  blue: [
+    "#010a16ff",
+    "#031227ff",
+    "#081b37ff",
+    "#0e2850ff",
+    "#122d56ff",
+    "#153565ff",
+  ],
+  lightBlue: [
+    "#011612ff",
+    "#032725ff",
+    "#0a7c72ff",
+    "#0f837fff",
+    "#14a3a3ff",
+    "#00f7ffff",
+  ],
+  red: [
+    "#160108ff",
+    "#27030cff",
+    "#4b0a21ff",
+    "#801136ff",
+    "#c12058ff",
+    "#f21759ff",
+  ],
+  orange: [
+    "#170b01ff",
+    "#291403ff",
+    "#391d06ff",
+    "#512908ff",
+    "#743909ff",
+    "#9c4e0dff",
+  ],
+  green: [
+    "#031701ff",
+    "#032904ff",
+    "#06390bff",
+    "#115108ff",
+    "#307409ff",
+    "#659c0dff",
+  ],
+};
+
+let wavesColors = colorSets.blue;
+// wavesColors = colorSets.lightBlue;
+// wavesColors = colorSets.red;
+// wavesColors = colorSets.orange;
+// wavesColors = colorSets.green;
 
 class DrawWaves {
   constructor(containerId, count) {
@@ -39,22 +79,27 @@ class DrawWaves {
     this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
     this.ctx.scale(dpr, dpr);
-    this.textSize = Math.max(this.width / 12, this.height / 6, 80);
+    this.textSize = Math.max(this.width / 20, this.height / 10, 30);
   }
 
   drawText() {
-    this.ctx.font = `${this.textSize}px monospace`;
-    this.ctx.fillStyle = "rgba(23, 146, 228, 0.4)";
+    this.ctx.font = `${this.textSize}px "Atkinson Hyperlegible Mono"`;
+    this.ctx.fillStyle = `rgba(250, 9, 89, 1)`;
     this.ctx.textAlign = "center";
     this.ctx.textBaseline = "middle";
+    this.ctx.letterSpacing = "28px";
     this.ctx.fillText(
-      "BOD",
+      Math.random() > 0.01 ? "BOD" : "B0D",
       this.width / 2,
       this.height / 2 - this.textSize * 0.8
     );
-    this.ctx.fillText("ZIO", this.width / 2, this.height / 2);
     this.ctx.fillText(
-      "NEK",
+      Math.random() > 0.01 ? "ZIO" : "5IO",
+      this.width / 2,
+      this.height / 2
+    );
+    this.ctx.fillText(
+      Math.random() > 0.01 ? "NEK" : "N3K",
       this.width / 2,
       this.height / 2 + this.textSize * 0.8
     );
@@ -75,7 +120,7 @@ class DrawWaves {
       this.canvas.style.height = `${containerRect.height}px`;
       this.canvas.style.width = `${containerRect.width}px`;
       this.ctx.scale(dpr, dpr);
-      this.textSize = Math.max(this.width / 14, this.height / 6, 80);
+      this.textSize = Math.max(this.width / 20, this.height / 10, 30);
     }
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.waves.forEach((wave) => wave.draw(this.ctx));
@@ -91,7 +136,8 @@ class DrawWaves {
           {
             color: wavesColors[i],
             speed: i + 1 * 1,
-            offset: that.height / 8 + i * 100,
+            offset: that.height / 10 + i * 100,
+            level: i,
           },
           that.width,
           that.height
@@ -104,8 +150,9 @@ class DrawWaves {
             {
               color: wavesColors[i],
               speed: i + 1 * 1,
-              offset: -that.height / 8 - i * 100,
+              offset: -that.height / 10 - i * 100,
               side: "top",
+              level: i,
             },
             that.width,
             that.height
@@ -120,6 +167,7 @@ class Wave {
   constructor(options, width, height) {
     this.color = options?.color || "#1A237E";
     this.speed = options?.speed || 0.2;
+    this.level = options?.level || 0;
     this.width = width;
     this.height = height;
     this.offset = options?.offset || 200;
@@ -127,7 +175,7 @@ class Wave {
     this.wavePointsCount =
       options?.wavePointsCount || Math.max(Math.floor(this.width / 100), 8);
     this.wavePoints = [];
-    this.waveHeight = 350;
+    this.waveHeight = 300;
     this.waveStep = this.width / this.wavePointsCount;
 
     this.wavePoints = [...Array(this.wavePointsCount + 4)].map((_, index) => [
@@ -173,6 +221,58 @@ class Wave {
         ctx.lineTo(this.width, this.side === "top" ? 0 : this.height);
       }
     }
+
+    ctx.closePath();
+    ctx.fill();
+
+    ctx.save();
+    ctx.beginPath();
+    ctx.moveTo(0, this.side === "top" ? 0 : this.height);
+    ctx.lineTo(this.wavePoints[0][0], this.wavePoints[0][1]);
+    const highlight = ctx.createRadialGradient(
+      this.width / 2,
+      this.height / 2,
+      200,
+      this.width / 2,
+      this.height / 2,
+      1000
+    );
+
+    highlight.addColorStop(
+      0.06,
+      `rgba(250, 9, 89, ${0.05 + this.level * 0.26})`
+    );
+    highlight.addColorStop(
+      0.1,
+      `rgba(250, 9, 89, ${0.05 + this.level * 0.16})`
+    );
+    highlight.addColorStop(0.4, "rgba(0, 0, 0, 0)");
+    highlight.addColorStop(1, "rgba(0, 0, 0, 1)");
+    ctx.fillStyle = highlight;
+
+    for (var i = 0; i < this.wavePoints.length; i++) {
+      if (this.wavePoints[i + 1]) {
+        ctx.quadraticCurveTo(
+          this.wavePoints[i][0],
+          this.wavePoints[i][1],
+          this.diff(this.wavePoints[i][0], this.wavePoints[i + 1][0]),
+          this.diff(this.wavePoints[i][1], this.wavePoints[i + 1][1])
+        );
+      } else {
+        ctx.lineTo(this.wavePoints[i][0], this.wavePoints[i][1]);
+        ctx.lineTo(this.width, this.side === "top" ? 0 : this.height);
+      }
+    }
+
+    ctx.closePath();
+    ctx.clip("evenodd");
+
+    ctx.beginPath();
+    ctx.arc(this.width / 2, this.height / 2, 2000, 0, Math.PI * 2);
+
+    ctx.fill();
+    ctx.restore();
+
     this.wavePoints.forEach((wp) => {
       wp[0] -= 1 * this.speed;
     });
@@ -192,8 +292,5 @@ class Wave {
         this.speed,
       ];
     }
-
-    ctx.closePath();
-    ctx.fill();
   }
 }
